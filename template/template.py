081f81b833c90c566cbf1a4c3b441a751a191445
ywcheong <tencise@gmail.com>
2024-12-24 13:49:04 +0900

    Fix: template.py 단순개선

--- template/template.py
+++ template/template.py
@@ -3,17 +3,9 @@
 import sys
 
 
-def input_one(given_type):
-    return given_type(sys.stdin.readline().strip())
-
-
-def input_list(given_type):
-    return list(map(given_type, input_one(str).split()))
-
-
-def solve(a):
-    """write your logic here"""
-    return None
+def solve(x):
+    """write your solution here"""
+    return x
 
 
 def test():
@@ -23,14 +15,22 @@ def test():
         else:
             print(f"Test fail: {left} != {right}")
 
-    """write your test here"""
-    check(1 + 1, 2)
+    """write your testing here"""
+    check(solve(1), 1)
+    check(solve(2), 2)
+    check(solve(3), 3)
 
 
 def main():
+    def input_one(given_type):
+        return given_type(sys.stdin.readline().strip())
+
+    def input_list(given_type):
+        return list(map(given_type, input_one(str).split()))
+
     """write your i/o here"""
-    a = input_one(int)
-    print(solve(a))
+    x = input_one(int)
+    print(solve(x))
 
 
 test()
