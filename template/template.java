5cf6c56c1ab57ab479768bf3d13d34a6701ebffc
ywcheong <tencise@gmail.com>
2025-01-03 09:05:18 +0900

    Feat: template.java 간소화

--- template/template.java
+++ template/template.java
@@ -1,69 +1,18 @@
 import java.io.*;
 import java.util.*;
 
+@SuppressWarnings("unused")
 public class Main {
-
-    private static class Reader {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        public int readInt() throws IOException {
-            String line = this.reader.readLine();
-            return Integer.parseInt(line);
-        }
-
-        public String readString() throws IOException {
-            return this.reader.readLine();
-        }
-
-        public int[] readIntList(String delim) throws IOException {
-            StringTokenizer tokenizer = new StringTokenizer(this.reader.readLine(), delim);
-            int[] intList = new int[tokenizer.countTokens()];
-
-            for (int i = 0; tokenizer.hasMoreTokens(); i++) {
-                intList[i] = Integer.parseInt(tokenizer.nextToken());
-            }
-
-            return intList;
-        }
-
-        public int[] readIntList() throws IOException {
-            return this.readIntList(" ");
-        }
-
-        public String[] readStringList() throws IOException {
-            StringTokenizer tokenizer = new StringTokenizer(this.reader.readLine(), " ");
-            String[] stringList = new String[tokenizer.countTokens()];
-
-            for (int i = 0; tokenizer.hasMoreTokens(); i++) {
-                stringList[i] = tokenizer.nextToken();
-            }
-
-            return stringList;
-        }
-    }
-
-    private static class Writer {
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        private <T> void writeln(T line) throws IOException {
-            writer.write(line.toString());
-            writer.write('\n');
-        }
-
-        private void print() throws IOException {
-            writer.flush();
-        }
-    }
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        /* reader input */
-        Reader reader = new Reader();
-        Writer writer = new Writer();
+        // Write input handling here
 
-        /* Write your code here */
+        // Write your logic here    
 
-
-        /* stream flush */
-        writer.print();
+        // Write output handling here
+        writer.newLine();
+        writer.flush();
     }
 }
